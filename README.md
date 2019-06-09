a4a2dcab287570c42fff9985af5b8acb18593f9a
KBeDeveloper <b.esteban.calisto@gmail.com>
2019-06-09 15:35:00 -0400

    Reset local repo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VFC App
+# VFConnect Javascript Application
 
 Un proyecto para *__INS7501__*
 
@@ -18,6 +18,39 @@ Un proyecto para *__INS7501__*
 
 [NPM](https://www.npmjs.com/)
 <br/>
-[React Native Cli](https://facebook.github.io/react-native/docs/getting-started)
-<br/>
-[JavaJDK - OpenJDK](http://openjdk.java.net/)
+[ElectronJS](https://github.com/electron/electron)
+
+## Instrucciones y advertencias
+
+### Consideraciones
+
+1. node_modules no incluido.
+2. Sólo se incluyen los archivos de base.
+3. Las dependencias de <code>electron@5.0.2</code> deben ser instaladas de manera local.
+
+### Instrucciones de instalación y ejecución
+
+**Clonación de repositorio (Git version >=1.7.10)**
+
+<code>$ git clone -b branch --single-branch 'https://github.com/TeaTeamSoft/VFConnect.git'</code><br>
+<code>$ cd VFConnect</code>
+
+**Instalar dependencias de ElectronJS**
+
+*Local*
+
+<code>$ npm install electron@5.0.2</code>
+
+*Global*
+
+<code>$ npm install -g electron@5.0.2</code>
+
+**Iniciar aplicación**
+
+*Con Electron instalado de forma local y global*
+
+<code>$ npm start</code>
+
+*Exclusivo para instalación global*
+
+<code>$ electron .</code>
--- main.js
+++ main.js
@@ -1,36 +1,56 @@
-import {app,BrowserWindow} from 'electron'
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
 
-let win = null
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
 
-function createWindow(){
-    // Initialize the window to our specified dimensions
-    win = new BrowserWindow({width: 800, height: 600});
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-    // Specify entry point
-    win.loadURL('https://teateamsoft.github.io/app-vfc/');
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
 
-    // Show dev tools
-    // Remove this line before distributing
-    win.webContents.openDevTools()
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
-    // Remove window once app is closed
-    win.on('closed', function () {
-        win = null;
-    });
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
 }
 
-app.on('ready', function () {
-    createWindow()
-})
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
-app.on('activate', () => {
-    if (win === null) {
-        createWindow()
-    }
-})
-
+// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    if (process.platform != 'darwin') {
-        app.quit()
-    }
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
